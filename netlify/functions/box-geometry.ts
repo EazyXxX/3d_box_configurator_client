078334d888b860547b2d294336efc1ce6e6b8724
Oleg Ostanin <104682962+EazyXxX@users.noreply.github.com>
2025-01-27 12:24:49 +0300

    Update translation in box-geometry.ts

--- netlify/functions/box-geometry.ts
+++ netlify/functions/box-geometry.ts
@@ -1,45 +1,45 @@
 import { BoxDimensions, Vertex, Triangle, BoxData } from "../../app/types";
 import { Handler, HandlerEvent } from "@netlify/functions";
 
-// Функция расчёта триангуляции коробки
+// Box triangulation calculation function
 function calculateBoxTriangulation(dimensions: BoxDimensions): BoxData {
   const { length: l, width: w, height: h } = dimensions;
 
-  // Определение 8 вершин коробки
+  // Defining the 8 vertices of the box
   const vertices: Vertex[] = [
-    { x: 0, y: 0, z: 0 }, // 0: передний нижний левый
-    { x: l, y: 0, z: 0 }, // 1: передний нижний правый
-    { x: l, y: h, z: 0 }, // 2: передний верхний правый
-    { x: 0, y: h, z: 0 }, // 3: передний верхний левый
-    { x: 0, y: 0, z: w }, // 4: задний нижний левый
-    { x: l, y: 0, z: w }, // 5: задний нижний правый
-    { x: l, y: h, z: w }, // 6: задний верхний правый
-    { x: 0, y: h, z: w }, // 7: задний верхний левый
+    { x: 0, y: 0, z: 0 }, // 0: Front bottom left
+    { x: l, y: 0, z: 0 }, // 1: Front bottom right
+    { x: l, y: h, z: 0 }, // 2: Front upper right
+    { x: 0, y: h, z: 0 }, // 3: Front upper left
+    { x: 0, y: 0, z: w }, // 4: Rear lower left
+    { x: l, y: 0, z: w }, // 5: Rear lower right
+    { x: l, y: h, z: w }, // 6: Rear upper right
+    { x: 0, y: h, z: w }, // 7: Rear upper left
   ];
 
-  // Определение 12 треугольников (по 2 на каждую грань)
+  // Definition of 12 triangles (2 for each face)
   const triangles: Triangle[] = [
-    // Передняя грань
+    // Front face
     { v1: vertices[0], v2: vertices[1], v3: vertices[2] },
     { v1: vertices[0], v2: vertices[2], v3: vertices[3] },
 
-    // Задняя грань
+    // Rear face
     { v1: vertices[5], v2: vertices[4], v3: vertices[7] },
     { v1: vertices[5], v2: vertices[7], v3: vertices[6] },
 
-    // Верхняя грань
+    // Upper face
     { v1: vertices[3], v2: vertices[2], v3: vertices[6] },
     { v1: vertices[3], v2: vertices[6], v3: vertices[7] },
 
-    // Нижняя грань
+    // Bottom face
     { v1: vertices[4], v2: vertices[5], v3: vertices[1] },
     { v1: vertices[4], v2: vertices[1], v3: vertices[0] },
 
-    // Правая грань
+    // Right face
     { v1: vertices[1], v2: vertices[5], v3: vertices[6] },
     { v1: vertices[1], v2: vertices[6], v3: vertices[2] },
 
-    // Левая грань
+    // Left face
     { v1: vertices[4], v2: vertices[0], v3: vertices[3] },
     { v1: vertices[4], v2: vertices[3], v3: vertices[7] },
   ];
